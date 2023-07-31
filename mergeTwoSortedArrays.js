9e97f37872967cbd621b448a347f780d8beda454
Souvik Kundu <kivtech88@gmail.com>
2023-07-31 19:32:15 +0530

    commit

---
+++ mergeTwoSortedArrays.js
@@ -0,0 +1,11 @@
+/*
+Problem Statement 
+Implement a function that merges two sorted arrays into another sorted array. Name it mergeArrays(arr1, arr2).
+
+Input: 
+Two sorted arrays.
+
+Output: 
+A merged sorted array consisting of all elements of both input arrays.
+*/
+
