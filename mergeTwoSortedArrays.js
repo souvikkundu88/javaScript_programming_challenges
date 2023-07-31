49b35f3baa414edfc29b26b016cf001d40ec219c
Souvik Kundu <kivtech88@gmail.com>
2023-07-31 19:35:13 +0530

    Update mergeTwoSortedArrays.js

--- mergeTwoSortedArrays.js
+++ mergeTwoSortedArrays.js
@@ -15,7 +15,8 @@ A merged sorted array consisting of all elements of both input arrays.
 function mergeArrays(arr1, arr2) {
     return [...arr1, ...arr2].sort((a, b) => a - b);
 }
-*
+*/
+
 console.log(mergeArrays([4,5,6],[-2,-1,0,7]))
 
 // solution 2
