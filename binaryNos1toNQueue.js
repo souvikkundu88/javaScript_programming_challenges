7bd6665051cd6f1b9ed8d66d3f0563f2b9ec433b
Souvik Kundu <kivtech88@gmail.com>
2023-08-01 22:55:27 +0530

    Update binaryNos1toNQueue.js

--- binaryNos1toNQueue.js
+++ binaryNos1toNQueue.js
@@ -1,3 +1,17 @@
+/*
+
+Problem Statement
+Implement a function findBin(n), which will generate binary numbers from 
+1 to n in the form of a string using a queue. An illustration is also provided for your understanding.
+
+Input
+A positive integer n
+
+Output
+Returns binary numbers in the form of strings from 1 up to n
+
+*/
+
 "use strict";
 
 function findBin(n) {
@@ -20,3 +34,36 @@ function findBin(n) {
 }
 
 console.log(findBin(10))
+
+/*
+module.exports = class Queue {
+  
+  constructor() {
+    this.items = new DoublyLinkedList();
+  }
+
+  isEmpty() {   
+    return this.items.length == 0;
+  }
+
+  getFront() {
+    if (!(this.isEmpty())) {
+     return this.items.getHead(); 
+  } else
+     return null;
+  }
+
+  size() {
+    return this.items.length;
+  }
+  
+  enqueue(element) {
+    return this.items.insertTail(element);
+  }
+
+  dequeue() {
+    return this.items.removeHead();
+  }
+
+}
+*/
